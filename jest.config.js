02d58312f542af532c748083218092cdde8c1393
Florian Traverse <florian.traverse@rocksolead.com>
2020-11-12 17:28:23 +0100

    chore: update dependency versions to match runtime

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,8 @@
 const {defaults: tsjPreset} = require('ts-jest/presets');
-const rtlPreset = require('@testing-library/react-native/jest-preset');
 
 module.exports = {
   ...tsjPreset,
-  preset: '@testing-library/react-native',
+  preset: 'react-native',
   transform: {
     ...tsjPreset.transform,
     '\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js'
@@ -17,8 +16,6 @@ module.exports = {
     '<rootDir>/src/__tests__/**/*.test.tsx',
     '<rootDir>/src/__tests__/**/*.test.ts'
   ],
-  setupFiles: [...rtlPreset.setupFiles],
-  setupFilesAfterEnv: ['@testing-library/react-native/cleanup-after-each'],
   // This is the only part which you can keep
   // from the above linked tutorial's config:
   cacheDirectory: '.jest/cache'
